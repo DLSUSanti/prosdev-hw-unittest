bf36db5cbe81461af8f22c2ed44a199a58ef29a4
DLSUSanti <santiago_alvarez@dlsu.edu.ph>
2020-07-21 23:27:55 +0800

    added code and unit test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const functions = {
   gradeClassifier: (grade) => {
-    // TODO: Write code here
+    if(grade >= 95 && grade <= 100) return 'O: Outstanding'
+    if(grade >= 90 && grade < 95) return 'V: Very Good'
+    if(grade >= 85 && grade < 90) return 'G: Good'
+    if(grade >= 80 && grade < 85) return 'S: Satisfactory'
+    if(grade >= 75 && grade < 80) return 'N: Needs Improvement'
+    if(grade >= 0 && grade < 75) return 'D: Did not Meet Expectation'
+    return 'Invalid user input.'
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,33 @@
 const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
-describe('Scenario', () => {
-  it('When ..., then ...', () => {
+describe('Letter Grade Module', () => {
+  it('The user inputs a number grade, then the output should be the corrseponding letter grade', () => {
     // Arrange
+      var testO = 98
+      var testV = 92
+      var testG = 86
+      var testS = 83
+      var testN = 79
+      var testD = 62
+      var testError = -1
 
     // Act
+      var outputO = gradeClassifier(testO)
+      var outputV = gradeClassifier(testV)
+      var outputG = gradeClassifier(testG)
+      var outputS = gradeClassifier(testS)
+      var outputN = gradeClassifier(testN)
+      var outputD = gradeClassifier(testD)
+      var outputError = gradeClassifier(testError)
 
     // Assert
+      expect(outputO).toEqual('O: Outstanding')
+      expect(outputV).toEqual('V: Very Good')
+      expect(outputG).toEqual('G: Good')
+      expect(outputS).toEqual('S: Satisfactory')
+      expect(outputN).toEqual('N: Needs Improvement')
+      expect(outputD).toEqual('D: Did not Meet Expectation')
+      expect(outputError).toEqual('Invalid user input.')
   });
 });
